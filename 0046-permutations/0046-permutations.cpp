274a20df92aa344f26efd95a4919c0a14ae19fdd
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-07-07 19:34:30 +0530

    Time: 3 ms (29.29%), Space: 10.7 MB (48.6%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    int fact(int n){
-        if(n <=1) return 1;
-        return n*fact(n-1);
-    }
+    vector<vector<int>> res;
+    void solve(int idx, vector<int> &nums){
+        if(nums.size() == idx){
+            res.push_back(nums);
+            return;
+        }
 
-    vector<int> nextPerm(vector<int> &nums){
-        int i=nums.size()-2;
-        int j = nums.size()-1;
-
-            while( i>= 0 && nums[i+1] <= nums[i]){
-                i--;
-            }
-            if(i>=0){
-                 while(nums[i] >=  nums[j]){
-                    j--;
-                }
-                swap(nums[i], nums[j]);
-            }
-
-            reverse(nums.begin()+i+1, nums.end());
-            return nums;
+        for(int i= idx;i<nums.size();i++){
+            swap(nums[i], nums[idx]);
+            solve(idx+1, nums);
+            swap(nums[i], nums[idx]);
+        }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int f = fact(nums.size());
-        vector<int> v;
-            vector<vector<int>> x;
-            v = nextPerm(nums);
-            x.push_back(v);
-        for(int i=0;i<f-1;i++){
-            v = nextPerm(v);
-            x.push_back(v);
-        }
-        return x;
+        solve(0, nums);
+        return res;
     }
 };
