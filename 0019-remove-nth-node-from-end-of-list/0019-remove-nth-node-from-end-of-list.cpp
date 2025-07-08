e2f7c5a6b70a807458467865d201331d995cf63b
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-07-09 00:44:01 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.8%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,22 +11,53 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head == NULL) return NULL;
-        int cnt = 0;
-        ListNode* temp = head;
-        while(temp != NULL){
-            cnt++;
-            temp = temp->next;
+        // Brute
+        // if(head == NULL) return NULL;
+        // int cnt = 0;
+        // ListNode* temp = head;
+        // while(temp != NULL){
+        //     cnt++;
+        //     temp = temp->next;
+        // }
+
+        // if (n == cnt) {
+        //     ListNode* newHead = head->next;
+        //     delete head;
+        //     return newHead;
+        // }
+        // if(n==1 && cnt==1 )return NULL;
+        // int sz = cnt-n;
+        // temp = head;
+        // for(int i=1;i<sz;i++){
+        //     temp = temp->next;
+        // }
+        // ListNode* dummy = temp->next;
+        // temp->next = temp->next->next;
+        // delete dummy;
+        // return head;
+
+        ListNode* f = head;
+        ListNode* s = head;
+
+        for(int i=0;i<n;i++){
+            f = f->next;
         }
-        if(n==1 && cnt==1 )return NULL;
-        int sz = cnt-n;
-        temp = head;
-        for(int i=1;i<sz;i++){
-            temp = temp->next;
+
+        if (f == nullptr) {
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
         }
-        ListNode* dummy = temp->next;
-        temp->next = temp->next->next;
-        delete dummy;
+
+        while(f->next!=NULL){
+            f = f->next;
+            s = s->next;
+        }
+
+        ListNode* toDelete = s->next;
+        s->next = s->next->next;
+        delete toDelete;
+
         return head;
     }
 };
