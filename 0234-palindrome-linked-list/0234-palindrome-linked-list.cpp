0ad3eb1c40b55a7fd9810635ae32d926799c787f
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-07-09 18:32:17 +0530

    Time: 0 ms (100%), Space: 118.1 MB (69.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,17 +10,60 @@
  */
 class Solution {
 public:
+    ListNode* revr(ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* prv = NULL;
+        while(head){
+            ListNode* fw = head->next;
+            head->next = prv;
+            prv = head;
+            head = fw;
+        }
+
+        return prv;
+    }
     bool isPalindrome(ListNode* head) {
-        string s= "";
+        // brute
+        // string s= "";
+        // ListNode* temp = head;
+        // while(temp != NULL){
+        //     s +=  to_string(temp->val);
+        //     temp = temp->next;
+        // }
+
+        // string x = s;
+        // reverse(s.begin(), s.end());
+        // if(s == x) return true;
+        // return false;
+
+
+        int sz = 0;
         ListNode* temp = head;
-        while(temp != NULL){
-            s +=  to_string(temp->val);
+        while(temp!=NULL){
+            sz++;
             temp = temp->next;
         }
 
-        string x = s;
-        reverse(s.begin(), s.end());
-        if(s == x) return true;
-        return false;
+        temp = head;
+        int mid = sz/2;
+        for(int i=0;i<mid;i++){
+            temp = temp->next;
+        }
+
+        if(sz%2 != 0) temp = temp->next;
+        ListNode* temp2 = temp;
+        temp = head;
+
+        ListNode* scn = revr(temp2);
+
+        for(int i=0;i<mid;i++){
+            if (temp->val != scn->val)
+                return false;
+            temp = temp->next;
+            scn = scn->next;
+        }
+
+        return true;
     }
 };
