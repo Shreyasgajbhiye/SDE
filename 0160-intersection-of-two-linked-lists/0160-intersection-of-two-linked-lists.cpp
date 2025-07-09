697db0a76152dcc1a21e8047808aa911028b36b9
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-07-09 16:35:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,21 +8,6 @@
  */
 class Solution {
 public:
-    ListNode* inter(ListNode* headA, ListNode* headB, int d){
-        ListNode* l1 = headA;
-        ListNode* l2 = headB;
-
-        while(d){
-            l1 = l1->next;
-            d--;
-        }
-
-        while(l1 != l2){
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        return l1;
-    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         // brute
         // map<ListNode*, int> mpp;
@@ -44,30 +29,60 @@ public:
         // }
         // return NULL;
 
-        //optimal
+        //better
+    // ListNode* inter(ListNode* headA, ListNode* headB, int d){
+    //     ListNode* l1 = headA;
+    //     ListNode* l2 = headB;
+
+    //     while(d){
+    //         l1 = l1->next;
+    //         d--;
+    //     }
+
+    //     while(l1 != l2){
+    //         l1 = l1->next;
+    //         l2 = l2->next;
+    //     }
+    //     return l1;
+    // }
 
 
-        int n1 = 0;
-        int n2 = 0;
-        ListNode* t1 = headA;
-        ListNode* t2 = headB;
+        // int n1 = 0;
+        // int n2 = 0;
+        // ListNode* t1 = headA;
+        // ListNode* t2 = headB;
 
-        while(t1 != NULL){
-            n1++;
-            t1 = t1->next;
-        }
+        // while(t1 != NULL){
+        //     n1++;
+        //     t1 = t1->next;
+        // }
 
-        while(t2 != NULL){
-            n2++;
-            t2 = t2->next;
-        }
+        // while(t2 != NULL){
+        //     n2++;
+        //     t2 = t2->next;
+        // }
         
-        if(n1 > n2){
-            return inter(headA, headB, n1-n2);
+        // if(n1 > n2){
+        //     return inter(headA, headB, n1-n2);
+        // }
+        // else{
+        //     return inter(headB, headA, n2-n1);
+        // }
+        // return NULL;
+
+        if(headA == NULL && headB == NULL) return NULL;
+        ListNode* l1 = headA;
+        ListNode* l2 = headB;
+
+        while(l1 != l2){
+            l1 = l1->next;
+            l2 = l2->next;
+            
+            if(l1 == l2) return l1;
+            if(l1 == NULL) l1 = headB;
+            if(l2 == NULL) l2 = headA;
         }
-        else{
-            return inter(headB, headA, n2-n1);
-        }
+
         return NULL;
     }
 };
