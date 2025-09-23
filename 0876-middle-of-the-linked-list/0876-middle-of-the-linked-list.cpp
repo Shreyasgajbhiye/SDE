ae187fe19875bd70fd752b1d2b8168e21b1a2756
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-09-23 22:58:32 +0530

    Time: 0 ms (100%), Space: 10 MB (25.6%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -26,13 +26,12 @@ public:
         // }
         // return temp;
 
-
         ListNode* s = head;
         ListNode* f = head;
-        while(f != NULL && f->next != NULL){
+        while( f != NULL && f->next != NULL){
             s = s->next;
             f = f->next->next;
-        }      
+        }
         return s;
     }
 };
