f9ea1cbfc84f535a86977c3bd162da65021bafa1
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-11-02 12:09:52 +0530

    Time: 3 ms (14.61%), Space: 19.7 MB (42.64%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,49 +12,53 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*,unsigned long long >> q;
+        // queue<pair<TreeNode*,unsigned long long >> q;
+        // q.push({root, 0});
+        // unsigned long long maxi = 0;
+        // while(!q.empty()){
+        //     unsigned long long l = q.front().second;
+        //     unsigned long long r = q.back().second;
+
+        //     maxi = max(maxi, r-l+1);
+
+        //     unsigned long long sz = q.size();
+
+        //     for(int i=0;i<sz;i++){
+        //         TreeNode* n = q.front().first;
+        //         unsigned long long idx = q.front().second;
+        //         q.pop();
+        //         if(n->left){
+        //             q.push({n->left, 2*idx+1});
+        //         }
+        //         if(n->right){
+        //             q.push({n->right, 2*idx+2});
+        //         }
+        //     }
+        // }
+        // return maxi;
+
+
+        queue<pair<TreeNode*, unsigned long long>> q;
+        unsigned long long width = 0;
         q.push({root, 0});
-        unsigned long long maxi = 0;
         while(!q.empty()){
-            unsigned long long l = q.front().second;
-            unsigned long long r = q.back().second;
-
-            maxi = max(maxi, r-l+1);
-
-            unsigned long long sz = q.size();
-
-            for(int i=0;i<sz;i++){
+            TreeNode* n = q.front().first;
+            unsigned long long f = q.front().second;
+            unsigned long long b = q.back().second;
+            width = max(width, b-f+1);
+            unsigned long long size = q.size();
+            for(unsigned long long i = 0;i<size;i++){
                 TreeNode* n = q.front().first;
-                unsigned long long idx = q.front().second;
+                unsigned long long f = q.front().second;
                 q.pop();
                 if(n->left){
-                    q.push({n->left, 2*idx+1});
+                    q.push({n->left, 2*f+1});
                 }
                 if(n->right){
-                    q.push({n->right, 2*idx+2});
+                    q.push({n->right,2*f+2} );
                 }
             }
         }
-        return maxi;
-
-
-//         queue<pair<TreeNode* , long long>> q;
-//         q.push({root, 0});
-//          long long width = 0;
-//         while(!q.empty()){
-//              long long l = q.front().second;
-//             long long r = q.back().second;
-//             width = max(width, r-l+1);
-//              long long sz = q.size();
-//             for( long long i=0;i<sz;i++){
-//                 TreeNode* node = q.front().first;
-//                  long long lv = q.front().second;
-//                 q.pop();
-
-//                 if(node->left) q.push({node->left, 2*lv+1});
-//                 if(node->right) q.push({node->right, 2*lv+2});
-//             }
-//         }
-// return width;
+        return width;
     }
 };
