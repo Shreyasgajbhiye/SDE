0821f85c7b3d851028d75907b792c02548cc3d60
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-11-22 23:43:56 +0530

    Time: 0 ms (100%), Space: 9.6 MB (18.7%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,60 +1,35 @@
 class MyStack {
 public:
-    // 2 queue
-    // queue<int> q1;
-    // queue<int> q2;
-    // MyStack() {
+    queue<int> q1;
+    queue<int> q2;
+    MyStack() {
         
-    // }
-    
-    // void push(int x) {
-    //     q2.push(x);
-    //     while(!q1.empty()){
-    //         q2.push(q1.front());
-    //         q1.pop();
-    //     }
-    //     swap(q1, q2);
-    // }
-    
-    // int pop() {
-    //     int result = q1.front();
-    //     q1.pop();
-    //     return result;
-    // }
-    
-    // int top() {
-    //     return q1.front();
-    // }
-    
-    // bool empty() {
-    //     return q1.empty();
-    // }
-    queue<int> q;
-    int size;
-     MyStack() {
-        size = 0;
     }
     
     void push(int x) {
-        q.push(x);
-        for(int i=0;i<q.size()-1;i++){
-            q.push(q.front());
-            q.pop();
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
         }
+        swap(q1, q2);
     }
     
     int pop() {
-        int result = q.front();
-        q.pop();
-        return result;
+        int x = q1.front();
+        q1.pop();
+        return x;
+
     }
     
     int top() {
-        return q.front();
+         int x = q1.front();
+        // q2.pop();
+        return x;
     }
     
     bool empty() {
-        return q.empty();
+        return q1.empty();
     }
 };
 
