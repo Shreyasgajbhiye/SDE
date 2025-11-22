c26ace0d368a5fe6cae0994cf34aaa6611c3853f
Shreyash Gajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2025-11-23 00:23:08 +0530

    Time: 219 ms (55.83%), Space: 146.9 MB (39.81%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    int partition(vector<int> &nums, int l, int r){
-        int pi = nums[l];
-        int i=l;
-        int j=r;
+    void merge(int low, int mid, int high, vector<int> &nums){
+        int i = low;
+        int j = mid+1;
+        int k =0;
+        vector<int> v(high-low+1);
+        while(i<=mid && j<=high){
+            if(nums[i] < nums[j]){
+                v[k++] = nums[i++];
+            }
+            else{
+                v[k++] = nums[j++];
+            }
+        }
+        while(i <= mid){
+            v[k++] = nums[i++]; 
+        }
 
-        while(i<j){
-            while(nums[i] <= pi && i<=r-1){
-                i++;
-            }
-            while(nums[j] > pi && j>=l+1){
-                j--;
-            }
-            if(i<j) swap(nums[i], nums[j]);
+        while(j <= high){
+            v[k++] = nums[j++]; 
         }
-        swap(nums[l], nums[j]);
-        return j;
+        for(int p =0;p<v.size();p++){
+            nums[p + low] = v[p];
+        }
+        
     }
-    void qs(vector<int> &nums, int l, int r){
-        if(l<r){
-            int part = partition(nums, l, r);
-            qs(nums, l, part-1 );
-            qs(nums, part+1, r);
-        }
+    void mergeSort(int low,int high, vector<int> &nums){
+        while(low >= high) return;
+            int mid =  low + (high - low) / 2;
+            mergeSort(low, mid, nums);
+            mergeSort(mid+1, high, nums);
+            merge(low, mid, high, nums);
+        
     }
     vector<int> sortArray(vector<int>& nums) {
-        qs(nums, 0, nums.size()-1);
-        return nums;
+        int low = 0;
+        int high = nums.size()-1;
+        int mid = high - low /2;
+        mergeSort(low, high, nums);
+
+       return nums;
     }
 };
